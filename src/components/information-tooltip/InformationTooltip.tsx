455af1b66d8a5b07ad843f16b60b9da20d96901d
Pantho <pantho.sarkar.7@gmail.com>
2025-03-03 14:23:30 +0600

    Bump version to 1.12.4 and add fillColor and borderColor props to InformationTooltip component

--- src/components/information-tooltip/InformationTooltip.tsx
+++ src/components/information-tooltip/InformationTooltip.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from "react";
 import { cn } from "../../lib/utils/cn";
+import { IconArrow, IconInfo } from "./icons";
 
 type TProps = {
   children: React.ReactNode;
   content: string;
   className?: string;
   align?: "left" | "right" | "top" | "bottom";
+  fillColor?: string;
+  borderColor?: string;
 };
 
 const InformationTooltip: FC<TProps> = ({
@@ -13,40 +16,90 @@ const InformationTooltip: FC<TProps> = ({
   content,
   className,
   align = "right",
+  fillColor,
+  borderColor,
 }) => {
   const positionStyles = {
-    left: align === "right" ? "100%" : align === "left" ? "auto" : "50%",
-    right: align === "left" ? "100%" : "auto",
-    top: align === "bottom" ? "100%" : align === "top" ? "auto" : "50%",
-    bottom: align === "top" ? "100%" : "auto",
+    left:
+      align === "right"
+        ? "calc(100% + 24%)"
+        : align === "left"
+        ? "auto"
+        : "50%",
+    right: align === "left" ? "calc(100% + 24%)" : "auto",
+    top:
+      align === "bottom"
+        ? "calc(100% + 20%)"
+        : align === "top"
+        ? "auto"
+        : "50%",
+    bottom: align === "top" ? "calc(100% + 19%)" : "auto",
     transform:
       align === "top" || align === "bottom"
         ? "translateX(-50%)"
         : "translateY(-50%)",
   };
 
-  const animationClasses = cn(
-    align === "top"
-      ? "origin-bottom scale-y-0 group-hover:scale-y-100"
-      : align === "bottom"
-      ? "origin-top scale-y-0 group-hover:scale-y-100"
-      : align === "left"
-      ? "origin-right scale-x-0 group-hover:scale-x-100"
-      : "origin-left scale-x-0 group-hover:scale-x-100"
-  );
+  // const animationClasses = cn(
+  //   align === "top"
+  //     ? "origin-bottom scale-y-0 group-hover:scale-y-100"
+  //     : align === "bottom"
+  //     ? "origin-top scale-y-0 group-hover:scale-y-100"
+  //     : align === "left"
+  //     ? "origin-right scale-x-0 group-hover:scale-x-100"
+  //     : "origin-left scale-x-0 group-hover:scale-x-100"
+  // );
+
+  const arrowStyles = {
+    left:
+      align === "right"
+        ? "calc(100% - 26px)"
+        : align === "top"
+        ? "16px"
+        : "auto", // Adjusts arrow position on the right
+    right:
+      align === "left"
+        ? "calc(100% - 27px)"
+        : align === "bottom"
+        ? "16px"
+        : "auto", // Adjusts arrow position on the left
+    top: align === "bottom" ? "8px" : "auto", // Adjusts arrow position on the top
+    bottom: align === "top" ? "8px" : "auto", // Adjusts arrow position on the bottom
+    transform:
+      align === "top"
+        ? "translateX(-50%) "
+        : align === "right"
+        ? "rotate(90deg) translateX(-100%)"
+        : align === "left"
+        ? "rotate(-90deg) translateX(100%)"
+        : align === "bottom"
+        ? "rotate(180deg) translateX(-50%)"
+        : "", // Centers arrow for top and bottom alignments
+  };
 
   return (
     <div className="relative group my-auto cursor-pointer">
-      {children}
+      {children ? children : <IconInfo />}
       <div
         className={cn(
-          "absolute bg-white border-primary-200 border text-sm text-primary shadow-md rounded-15 w-56 px-4 py-3 opacity-0 group-hover:opacity-100 transition-all z-10",
-          animationClasses,
-          className
+          "absolute border text-sm text-primary shadow-md rounded-15 w-56 px-4 py-3 hidden group-hover:block transition-all z-10",
+          // animationClasses,
+          className,
+          fillColor ? `bg-${fillColor}` : "bg-[#FFF9DB]",
+          borderColor ? `border-${borderColor}` : "border-[#FFE6BA]"
         )}
         style={positionStyles}
       >
-        {content}
+        <span>{content}</span>
+      </div>
+      <div
+        className="absolute size-full hidden group-hover:block z-10"
+        style={arrowStyles}
+      >
+        <IconArrow
+          fillColor={fillColor || "#FFF9DB"}
+          borderColor={borderColor || "#FFE6BA"}
+        />
       </div>
     </div>
   );
