140a51199e9de16317e10051ed989479c5b57c22
Pantho <pantho.sarkar.7@gmail.com>
2025-03-23 12:26:29 +0600

    ButtonWrapper Added 1.16.2

--- src/components/button-wrapper/ButtonWrapper.tsx
+++ src/components/button-wrapper/ButtonWrapper.tsx
@@ -1,28 +1,66 @@
 import React from "react";
 import "./style.css";
 import { cn } from "../../lib/utils/cn";
+import { iconLoading } from "./icons/icon";
+import style from "./style/style.module.css";
 
 interface ButtonWrapperProps {
+  ImageComponent: any;
   children: React.ReactNode;
-  active?: boolean;
   className?: string;
   onClick?: () => void;
+  blockIcon: string;
+  hoverIcon: string;
+  loading: boolean;
+  disabled: boolean;
 }
 
+export const ButtonLoading: React.FC = () => {
+  return (
+    <div className={cn(style.btnLoadingWrapper, "btnLoadingWrapper")}>
+      <span className={cn(style.btnContent, "btnContent")}>
+        {iconLoading()}
+      </span>
+    </div>
+  );
+};
+
 const ButtonWrapper: React.FC<ButtonWrapperProps> = ({
-  children,
-  active,
   className,
   onClick,
+  ImageComponent,
+  blockIcon,
+  hoverIcon,
+  loading,
+  disabled,
 }) => {
   return (
     <button
-      className={cn("hover_icon", className, {
-        active: active,
-      })}
+      className={cn(style.btnClass, className)}
       onClick={onClick}
+      disabled={disabled}
     >
-      {children}
+      <ImageComponent
+        src={blockIcon}
+        width={100}
+        height={100}
+        alt="sync"
+        sizes="100vw"
+        className={cn(style.blockIcon, "blockIcon")}
+      />
+      <ImageComponent
+        src={hoverIcon}
+        width={100}
+        height={100}
+        alt="sync"
+        sizes="100vw"
+        className={cn(style.hoverIcon, "hoverIcon")}
+      />
+      {loading ? (
+        <div className={cn(style.btnLoading, "btnLoading")}>
+          <ButtonLoading />
+        </div>
+      ) : null}
     </button>
   );
 };
---
+++ src/components/button-wrapper/icons/icon.tsx
@@ -0,0 +1,27 @@
+export const iconLoading = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-loader-circle"
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56">
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from="0 12 12"
+          to="360 12 12"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+    </svg>
+  );
+};
